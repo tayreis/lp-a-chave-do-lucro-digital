113aceb53d2b836e7ef507279bb34a45937f11b6
tayreis <taynareis07@gmail.com>
2023-09-27 15:03:09 -0300

    Made site responsive for 1280px screens

--- src/assets/components/Footer.tsx
+++ src/assets/components/Footer.tsx
@@ -2,8 +2,8 @@ import styles from "../styles/footer.module.css"
 
 export default function Footer(){
     return (
-        <div>
-            <p className={styles.container}>Copyright © 2023 - Todos os Direitos Reservados.</p>
+        <div className={styles.container}>
+            <p >Copyright © 2023 - Todos os Direitos Reservados.</p>
         </div>
     )
 }
