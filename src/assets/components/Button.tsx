235a58efc7e9e35466b350c001b07e527a5bc727
tayreis <taynareis07@gmail.com>
2023-10-02 18:25:27 -0300

    Updated button links

--- src/assets/components/Button.tsx
+++ src/assets/components/Button.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import { buttons } from "../../json/db.json"
 import  styles from "../styles/buttons.module.css"
 
@@ -5,8 +6,8 @@ var btn = JSON.parse('{"title": "Clique e comece sua Transformação Digital!"}'
 
 export default function Button() {
     return (
-            <div className={styles.container}>
+            <Link href="#CTA" className={styles.container}>
                 <button className={styles.btn3}>{btn.title}</button>
-            </div>
+            </Link>
     )
 }
--- src/assets/components/ButtonTestimonials.tsx
+++ src/assets/components/ButtonTestimonials.tsx
@@ -1,12 +1,12 @@
-import { buttons } from "../../json/db.json"
+import Link from "next/link"
 import  styles from "../styles/buttons.module.css"
 
 var btn = JSON.parse('{"title": "Junte-se aos Vencedores! Adquira Já!"}')
 
 export default function ButtonTestimonials() {
     return (
-            <div>
+            <Link href="#CTA">
                 <button className={styles.btn3}>{btn.title}</button>
-            </div>
+            </Link>
     )
 }
